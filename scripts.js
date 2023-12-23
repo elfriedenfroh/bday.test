a437e48317efe162151ed0e223ebb8a773094db4
elfriedenfroh <154669236+elfriedenfroh@users.noreply.github.com>
2023-12-23 16:02:59 +0100

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,20 @@
+// Countdown bis zum 29.03.2024, 00:00 Uhr
+const countDownDate = new Date("Mar 29, 2024 00:00:00").getTime();
+
+const x = setInterval(function() {
+  const now = new Date().getTime();
+  const distance = countDownDate - now;
+
+  const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  document.getElementById("countdown").innerHTML = days + "d " + hours + "h "
+  + minutes + "m " + seconds + "s ";
+
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("countdown").innerHTML = "Juhu, endlich 18!";
+  }
+}, 1000);
